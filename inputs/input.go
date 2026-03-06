1fd78028541cbec88874e4c246e711b6d89cf8bf
behzod <bek.urozov@mail.ru>
2026-03-06 15:41:40 +0500

    new routes

--- inputs/input.go
+++ inputs/input.go
@@ -2,32 +2,31 @@ package inputs
 
 type Input struct {
 	Type           string     `json:"type"` //combo-box,date-time,text-view,number-view,check-box,label,search-view,text-field, hidden, auto-complete,file-uploader,button
-	Name           string     `json:"name"`
-	FromName       string     `json:"fromName"`
-	ReadOnly       bool       `json:"readOnly"`
-	Text           string     `json:"text"`
-	MaxLength      int        `json:"maxLength"`
-	MinLength      int        `json:"minLength"`
-	IsDefault      bool       `json:"isDefault"`
-	MetaData       string     `json:"metaData"` //search-view uchun malumot boradi qimat
-	MetaKey        string     `json:"metaKey"`  //search-view uchun malumot boradi kalit
-	ValidMessage   string     `json:"validMessage"`
-	Format         string     `json:"format"`
-	Items          ComboItems `json:"items"`
-	ClearNodes     []string   `json:"clearNodes"`
-	CompleteNodes  []string   `json:"completeNodes"`
-	InfoSource     string     `json:"infoSource"` // <id,text>
+	Name           string     `json:"name,omitempty"`
+	FromName       string     `json:"fromName,omitempty"`
+	ReadOnly       bool       `json:"readOnly,omitempty"`
+	Text           string     `json:"text,omitempty"`
+	MaxLength      int        `json:"maxLength,omitempty"`
+	MinLength      int        `json:"minLength,omitempty"`
+	IsDefault      bool       `json:"isDefault,omitempty"`
+	MetaData       string     `json:"metaData,omitempty"` //search-view uchun malumot boradi qimat
+	MetaKey        string     `json:"metaKey,omitempty"`  //search-view uchun malumot boradi kalit
+	ValidMessage   string     `json:"validMessage,omitempty"`
+	Format         string     `json:"format,omitempty"`
+	Items          ComboItems `json:"items,omitempty"`
+	ClearNodes     []string   `json:"clearNodes,omitempty"`
+	CompleteNodes  []string   `json:"completeNodes,omitempty"`
+	InfoSource     string     `json:"infoSource,omitempty"` // <id,text>
 	ItemsSource    string     `json:"itemsSource,omitempty"`
-	SearchName     string     `json:"searchObject"`
-	DefaultValue   string     `json:"defaultValue"`
-	Search         string     `json:"searchSource"`
-	DataType       string     `json:"dataType"` //number,string,bool : default string
-	Visible        string     `json:"visible"`
-	Template       string     `json:"template"` // {} ба мегирим параметроя
-	FileSource     string     `json:"fileSource"`
-	FileExtensions []string   `json:"fileExtensions"`
-	FileMaxSize    int        `json:"fileMaxSize"` // byte кати
-	Access         []string   `json:"access,omitempty"`
+	SearchName     string     `json:"searchObject,omitempty"`
+	DefaultValue   string     `json:"defaultValue,omitempty"`
+	Search         string     `json:"searchSource,omitempty"`
+	DataType       string     `json:"dataType,omitempty"` //number,string,bool : default string
+	Visible        string     `json:"visible,omitempty"`
+	Template       string     `json:"template,omitempty"` // {} ба мегирим параметроя
+	FileSource     string     `json:"fileSource,omitempty"`
+	FileExtensions []string   `json:"fileExtensions,omitempty"`
+	FileMaxSize    int        `json:"fileMaxSize,omitempty"` // byte кати
 }
 
 type ComboItem struct {
@@ -39,8 +38,4 @@ type ComboItems []ComboItem
 
 func (i Input) GetName() string {
 	return i.Text
-}
-
-func (i Input) GetAccess() []string {
-	return i.Access
 }
